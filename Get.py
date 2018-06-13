78faa3a16c8c7f83e0798980292cd223b9de68f5
Popcore <smallzed@gmail.com>
2018-06-13 11:55:45 +0100

    re introduce intro comment

--- Get.py
+++ Get.py
@@ -1,3 +1,13 @@
+# This code reads from the Thingful node, gets  list of all Sensors
+# The code then reads through the list of sensors and counts the number with a location of
+# zero (at the moment assumes if x=0 y=0)
+# Then displays percentage that are zero locations
+# Documentation for GROW node
+# https://growobservatory.github.io/ThingfulNode/#tag/Locations
+#
+#
+#
+
 import requests, json, sys
 # secret contains API Key.  Not in Github repo !
 # expects  AuthCode='API Key obtained from Thingful'
